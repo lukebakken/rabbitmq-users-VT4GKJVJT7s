2096d7683500a2cef714f8aff6a86e2362d5669b
Luke Bakken <luke@bakken.io>
2024-05-04 17:37:23 -0700

    tweaks

---
+++ run-openssl-client.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+set -eu
+
+readonly curdir="$(CDPATH='' cd -- "$(dirname -- "$0")" && pwd)"
+readonly tlshost="${1:-*}"
+readonly tlsport="${2:-8883}"
+
+readonly certs_dir="$curdir/certs"
+readonly ca_file="$certs_dir/ca_certificate.pem"
+readonly cert_file="$certs_dir/client_prokofiev.bakken.io_certificate.pem"
+readonly key_file="$certs_dir/client_prokofiev.bakken.io_key.pem"
+
+openssl s_client -connect "$tlshost:$tlsport" -CAfile "$ca_file" -cert "$cert_file" -key "$key_file" -verify 8
--- run-rabbitmq.sh
+++ run-rabbitmq.sh
@@ -7,13 +7,13 @@ readonly rabbitmq_version='3.13.2'
 readonly rabbitmq_etc="$curdir/rabbitmq_server-$rabbitmq_version/etc/rabbitmq"
 readonly rabbitmq_sbin="$curdir/rabbitmq_server-$rabbitmq_version/sbin"
 
-sed "s|@@PWD@@|$PWD|" "$curdir/rabbitmq.conf.in" > "$rabbitmq_etc/rabbitmq.conf"
-
 if [ ! -d "$curdir/rabbitmq_server-$rabbitmq_version" ]
 then
     curl -LO "https://github.com/rabbitmq/rabbitmq-server/releases/download/v$rabbitmq_version/rabbitmq-server-generic-unix-$rabbitmq_version.tar.xz"
     tar xf "rabbitmq-server-generic-unix-$rabbitmq_version.tar.xz"
-    "$rabbitmq_sbin/rabbitmq-plugins" enable rabbitmq_management
 fi
+
+sed -e "s|@@PWD@@|$curdir|" "$curdir/rabbitmq.conf.in" > "$rabbitmq_etc/rabbitmq.conf"
+cp -vf "$curdir/rmq/enabled_plugins" "$rabbitmq_etc/enabled_plugins"
 
 "$rabbitmq_sbin/rabbitmq-server"
